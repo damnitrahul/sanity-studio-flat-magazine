39d802c259a6f9eade3e2991888cc1070e202b57
Rahul Raj <damnitrahul@gmail.com>
2020-06-07 23:36:47 +0530

    netlify hook

--- src/dashboardConfig.js
+++ src/dashboardConfig.js
@@ -11,20 +11,20 @@ export default {
               description:
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
+                // {
+                //   buildHookId: '5ea93e0055e7e6bb36786eaf',
+                //   title: 'Sanity Studio',
+                //   name: 'sanity-gatsby-blog-studio-nhfx2jny',
+                //   apiId: '53fb2bd5-52d4-48d2-8423-d71f0fd57553'
+                // },
                 {
-                  buildHookId: '5ea93e0055e7e6bb36786eaf',
-                  title: 'Sanity Studio',
-                  name: 'sanity-gatsby-blog-studio-nhfx2jny',
-                  apiId: '53fb2bd5-52d4-48d2-8423-d71f0fd57553'
-                },
-                {
-                  buildHookId: '5ea93e00b5370a16095aec7c',
+                  buildHookId: '5edd2ba57874520905575388',
                   title: 'Blog Website',
-                  name: 'sanity-gatsby-blog-web-1bhgjdai',
+                  name: 'sanityadmin',
                   apiId: '44a2cb72-ab82-4573-94e6-381fb17d5ac7'
                 }
               ]
-            }
+            } // https://api.netlify.com/build_hooks/5edd2ba57874520905575388
           }
         ],
         data: [
@@ -33,7 +33,11 @@ export default {
             value: 'https://github.com/damnitrahul/sanity-gatsby-blog',
             category: 'Code'
           },
-          { title: 'Frontend', value: 'https://sanity-gatsby-blog-web-1bhgjdai.netlify.app', category: 'apps' }
+          {
+            title: 'Frontend',
+            value: 'https://sanity-gatsby-blog-web-1bhgjdai.netlify.app',
+            category: 'apps'
+          }
         ]
       }
     },
